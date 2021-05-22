a1ab0e3ba0b2fb22343b8cfcb21488c0ab1f4009
attila <attilavigh.contact@gmail.com>
2021-05-22 20:56:42 +0100

    Exercise A
    Create a variable called "test" and assign a string to it.

--- homework.js
+++ homework.js
@@ -1,2 +1,18 @@
 const movies = require('./data/movies');
-console.log(movies);
+
+//JS Basics
+
+/* Ex.A
+   Create a variable called "test" and assign a string to it.
+*/
+
+let test = "First test"
+
+const testExerciseA = () => {
+    console.log(test);
+}
+const testAllExercise = () => {
+
+    testExerciseA()
+};
+testAllExercise()
