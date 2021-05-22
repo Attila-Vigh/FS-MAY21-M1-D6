eb4b8c2cd76ebd80820a30937c4ff0ac6a8bafcf
attila <attilavigh.contact@gmail.com>
2021-05-22 21:17:03 +0100

    Exercise B done.
    Create a variable called "sum" and assign to it the result of the sum between 10 and 20.

--- homework.js
+++ homework.js
@@ -1,6 +1,6 @@
 const movies = require('./data/movies');
 
-//JS Basics
+// JS Basics
 
 /* Ex.A
    Create a variable called "test" and assign a string to it.
@@ -11,8 +11,19 @@ let test = "First test"
 const testExerciseA = () => {
     console.log(test);
 }
+
+/* Ex.B
+    Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
+*/
+
+let sum = 10 + 20
+
+const testExerciseB = () => {
+    console.log("sum:", sum); // => 30
+}
 const testAllExercise = () => {
 
-    testExerciseA()
+    // testExerciseA()
+    testExerciseB()
 };
 testAllExercise()
