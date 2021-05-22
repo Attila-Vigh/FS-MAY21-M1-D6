2e310871ecfb7f420edc7496ea80a78210ff225d
attila <attilavigh.contact@gmail.com>
2021-05-22 21:19:42 +0100

    Exercise C done.
    Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).

--- homework.js
+++ homework.js
@@ -21,9 +21,21 @@ let sum = 10 + 20
 const testExerciseB = () => {
     console.log("sum:", sum); // => 30
 }
+
+/* Ex.C 
+    Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+*/
+
+let random =  Math.floor(Math.random() * 20)
+
+const testExerciseC = () => {
+    console.log("random:", random); // =>
+}
+
 const testAllExercise = () => {
 
     // testExerciseA()
-    testExerciseB()
+    // testExerciseB()
+    testExerciseC()
 };
 testAllExercise()
